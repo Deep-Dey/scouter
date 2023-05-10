2a5616396d576e99b275a2c88d6e4511279f4045
Deep-Dey <deepdey20998@gmsil.com>
2023-05-10 21:38:48 +0530

    add minet class

--- dataset/choose_dataset.py
+++ dataset/choose_dataset.py
@@ -2,6 +2,7 @@ from dataset.mnist import MNIST
 from dataset.CUB200 import CUB_200
 from dataset.ConText import ConText, MakeList, MakeListImage
 from dataset.transform_func import make_transform
+from dataset.minet import Minet
 
 
 def select_dataset(args):
@@ -20,6 +21,12 @@ def select_dataset(args):
         return dataset_train, dataset_val
     if args.dataset == "ImageNet":
         train, val = MakeListImage(args).get_data()
+        dataset_train = ConText(train, transform=make_transform(args, "train"))
+        dataset_val = ConText(val, transform=make_transform(args, "val"))
+        return dataset_train, dataset_val
+    
+    if args.dataset == "MINET":
+        train, val = Minet(args).get_data()
         dataset_train = ConText(train, transform=make_transform(args, "train"))
         dataset_val = ConText(val, transform=make_transform(args, "val"))
         return dataset_train, dataset_val
